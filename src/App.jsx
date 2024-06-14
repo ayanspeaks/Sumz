1fb82cc294fb60b2bdcd2b75b684e6beabc751a7
ayanspeaks <ayans.2003kali@gmail.com>
2024-06-14 16:32:45 +0530

    your ai summerizer

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+        <div className="main">
+            <div className='gradientt' />
+        </div>
+
+        <div className='app'>
+            <Hero />
+            <Demo />
+        </div>
+    </main>
+  )
+}
+
+export default App
